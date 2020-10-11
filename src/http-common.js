996e60a847a2f6c671f439550bc90c2bfc4e21af
mrmuke <m.xingster@gmail.com>
2020-10-11 16:29:30 -0500

    hi

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 export default axios.create({
-	baseURL: /* "http://localhost:8080/api" */ "http://54.177.140.153.xip.io/api",
+	baseURL: /* "http://localhost:8080/api" */ "https://lfawa-api.herokuapp.com/api",
   headers:{
     "Content-type":"application/json",
     "token":localStorage.getItem("token")
--- src/services/UserDataService.js
+++ src/services/UserDataService.js
@@ -4,7 +4,7 @@ import http from "../http-common";
 class UserDataService {
     create(data) {
         return axios.create({
-		baseURL:/*  "http://localhost:8080/api" */ "http://54.177.140.153.xip.io/api",
+		baseURL:/*  "http://localhost:8080/api" */ "https://lfawa-api.herokuapp.com/api",
             headers: {
               "Content-type": "application/json",
               "id":data
