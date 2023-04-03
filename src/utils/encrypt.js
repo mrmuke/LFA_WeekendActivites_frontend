3b7df5190d12a049a2f24931e3715bed34d1b17b
Michael Xing <student@6094.local>
2023-04-03 13:01:01 -0500

    fixed encryption issues

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -5,12 +5,13 @@ export function encrypt(data){
     return CryptoJS.AES.encrypt(JSON.stringify(data),key).toString()
 }
 export function decrypt(data){
-    if(data ==null){
+    if(data ==null||data=="null"){
         return null;
     }
     if(data[0]=="{"){
         localStorage.setItem("user",encrypt(JSON.parse(data)))
         return JSON.parse(data)
       }
+      console.log(CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(data,key)));
     return JSON.parse(CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(data,key)))
 }
